8709688568216a12ee4a4cc1acca95ad422d5d73
lukaskellerstein <lukas.kellerstein@gmail.com>
2024-03-11 22:09:22 +0100

    add fine tuning optimization for windows

--- 6_hugging-face/2_inference_optimization/Win/3_text-generation/bitsandbytes/1_example-gpu-4bit/main.py
+++ 6_hugging-face/2_inference_optimization/Win/3_text-generation/bitsandbytes/1_example-gpu-4bit/main.py
@@ -28,8 +28,9 @@ quant_config = BitsAndBytesConfig(
 model_4bit = AutoModelForCausalLM.from_pretrained(
     model_name, device_map="auto", quantization_config=quant_config
 )
+
 # ValueError: `.to` is not supported for `4-bit` or `8-bit` bitsandbytes models. Please use the model as it is, since the model has already been set to the correct devices and casted to the correct `dtype`.
-# model_4bit = model_4bit.to("mps")
+# model_4bit = model_4bit.to("cuda")
 
 # Create a pipeline for text generation
 my_pipeline = pipeline("text-generation", model=model_4bit, tokenizer=tokenizer)
---
+++ 6_hugging-face/4_fine-tuning_optimization/Win/3_text-generation/1_example-gpu-4bit/main-using.py
@@ -0,0 +1,32 @@
+import time
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+
+start = time.time()
+
+# Load the trained model and tokenizer
+model = AutoModelForCausalLM.from_pretrained("SAVED_MODEL")
+model = model.to("cuda")
+
+tokenizer = AutoTokenizer.from_pretrained("SAVED_TOKENIZER")
+
+# Create a pipeline for text generation
+my_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
+
+# Your new sentences
+data = [
+    {"role": "user", "content": "Which is bigger, the moon or the earth?"},
+    {"role": "user", "content": "Which is bigger, a atom or a virus?"},
+]
+
+# Use the model to generate responses
+for chat in data:
+    input_text = chat["content"]
+    generated_text = my_pipeline(
+        input_text, pad_token_id=my_pipeline.tokenizer.eos_token_id, max_new_tokens=50
+    )[0]["generated_text"]
+    print(f"User: {input_text}\nAssistant: {generated_text}\n")
+
+
+end = time.time()
+print(f"NN takes: {end - start} sec.")
